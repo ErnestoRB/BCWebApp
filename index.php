0537b138d314b596c5237b6f1ed3ab345241dcae
Ernesto <ernestorb_@outlook.com>
2022-05-11 20:54:14 -0500

    Seguir con validación

--- index.php
+++ index.php
@@ -21,11 +21,11 @@
 			<form method="post" action="submited.php">
 				<fieldset>
 					<legend>Complete sus datos correctamente </legend>
-					Nombre(s): <input name="nombre" id="nombre" type="text">
-					Apellido paterno: <input name="apellido1" id="apellido1" type="text">
+					Nombre(s):* <input name="nombre" id="nombre" type="text" required>
+					Apellido paterno:* <input name="apellido1" id="apellido1" type="text" required>
 					Apellido materno: <input name="apellido2" id="apellido2" type="text">
-					<br><br> e-mail: <input type="email" id="email" name="email" pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$">
-					Edad:
+					<br><br> E-mail:* <input type="email" id="email" name="email" pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$" required>
+					Edad:*
 					<select id="edad" name="edad">
 						<?php
 							for($i=18; $i<100; $i++){
@@ -35,16 +35,18 @@
 						?>
 					</select>
 					<br><br>
-					Calle: <input name="calle" id="calle" type="text">
-					Interior: <input id="interior" id="interior" name="interior" type="number">
-					Colonia: <input name="colonia" id="colonia" type="text">
+					Calle:* <input name="calle" id="calle" type="text" required>
+					Interior:* <input id="interior" id="interior" name="interior" type="number" required>
+					Colonia:* <input name="colonia" id="colonia" type="text" required>
 					<br><br>
-					Telefono: <input id="telefono" id="telefono" name="telefono" type="number">
-					
+					Telefono:* <input type="tel" name="telefono" id="telefono" maxLength="10" pattern="^\d{10}$" required>
 				<div class="centrar"> 
 					<br><input type="submit" value="Enviar datos">
 				</fieldset>
 			</form>
+			<b>
+			* Campos necesarios
+			</b>
 			</div>
 		</div>
  	</body>
