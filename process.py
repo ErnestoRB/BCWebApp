22cad258f8b3c949f29930db2fb6cb0dfadece46
root <ender12as@gmail.com>
2022-05-28 13:03:43 -0500

    Script de guardado de datos y cambios menores

--- process.py
+++ process.py
@@ -2,15 +2,15 @@ import sys
 import os
 import errno
 
-#datos_usuario = sys.argv[1]
+datos_usuario = sys.argv[1]
 
-try:
+try: #crea nueva carpeta para guardar los datos, ignora si ya existe
 	os.mkdir('users_data')
 except OSError as e:
 	if e.errno != errno.EEXIST:
 		raise
 
 archivo = open('/var/www/html/BCWebApp/users_data/data.txt', 'a')
-archivo.write('hola' + '\n')
+archivo.write(datos_usuario + '\n')
 archivo.close()
 
